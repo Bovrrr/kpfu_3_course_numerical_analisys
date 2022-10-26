c6a32153287405d1a66d45bebaa4e9418b6518e6
Baurzhan Onbayev <baurzhanonbaev@gmail.com>
2022-10-26 11:42:23 +0300

    2.

--- control_work_1/variables.py
+++ control_work_1/variables.py
@@ -1,5 +1,18 @@
 import numpy as np
 
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def plot_graphic(fs, a, b, n):
+    f, ax = plt.subplots(1, 1)
+    f.set_size_inches(16, 9)
+    xs = np.linspace(a, b, n)
+    for f in fs:
+        ys = np.array([f(x) for x in xs])
+        sns.lineplot(x=xs, y=ys, ax=ax)
+    plt.show()
+
 
 def f(x):
     return 2 * np.log(x + 5 ** (1 / 2)) * np.sinh(x)
