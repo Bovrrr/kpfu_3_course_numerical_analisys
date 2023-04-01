af008b298bed993f69df4aa9b50193d66de25a39
Baurzhan Onbayev <baurzhanonbaev@gmail.com>
2023-04-01 19:57:11 +0300

    solved method Seidel 2 variant and gradient descent for linear equations

---
+++ 2_sem_cw_1/utils.py
@@ -0,0 +1,40 @@
+import numpy as np
+import torch as t
+from copy import deepcopy
+
+
+def norm_1_vec(x):
+    x = np.array(deepcopy(x))
+    return np.max(np.abs(x))
+
+
+def norm_2_vec(x):
+    x = np.array(deepcopy(x))
+    return np.sum(np.abs(x))
+
+
+def norm_3_vec(x):
+    x = np.array(deepcopy(x))
+    return np.sqrt(np.sum(x**2))
+
+
+def norm_1(A):
+    A = np.array(deepcopy(A))
+    return np.max(np.sum(np.abs(A), axis=1))
+
+
+def norm_2(A):
+    A = np.array(deepcopy(A))
+    return np.max(np.sum(np.abs(A), axis=0))
+
+
+def norm_3(A):
+    A = np.array(deepcopy(A))
+    return np.sqrt(norm_spectral(A.T @ A))
+
+
+def norm_spectral(A):
+    A = np.array(deepcopy(A))
+    eig_values = np.linalg.eigvals(A)
+    eig_values_norms = np.array([np.linalg.norm(elem) for elem in eig_values])
+    return np.max(eig_values_norms)
