0f84001c3fdd34d3e6206269961e5a98795d73a8
Baurzhan Onbayev <baurzhanonbaev@gmail.com>
2023-04-06 08:23:03 +0300

    last try

--- 2_sem_cw_1/utils.py
+++ 2_sem_cw_1/utils.py
@@ -29,7 +29,7 @@ def norm_2(A):
 
 def norm_3(A):
     A = np.array(deepcopy(A))
-    return np.sqrt(norm_spectral(A.T @ A))
+    return np.sqrt(norm_spectral(A @ A.T))
 
 
 def norm_spectral(A):
@@ -42,6 +42,36 @@ def norm_spectral(A):
 def count_norms(A):
     norm_func_collection = [norm_1, norm_2, norm_3]
     norm_values = [norm_func_collection[i](A) for i in range(len(norm_func_collection))]
-    for i, value in enumerate(norm_values):
-        print(f"{i+1}-ая норма: {value}")
+    # for i, value in enumerate(norm_values):
+    #     print(f"{i+1}-ая норма: {value}")
     return np.array(norm_values)
+
+
+def make_B_b(A, y):
+    B = -np.copy(A)
+    for i in range(B.shape[0]):
+        B[i, i] += 1
+    b = np.copy(y)
+    return B, b
+
+
+def make_F_H(B):
+    F = np.triu(B)
+    H = B - F
+    return F, H
+
+
+def make_B__b_(F, H, b):
+    n = F.shape[0]
+    B_ = np.linalg.inv(np.eye(n) - H) @ F
+    b_ = np.linalg.inv(np.eye(n) - H) @ b
+    return B_, b_
+
+
+def check_norm(A, y, D):
+    A, y = D @ A, D @ y
+    B, b = make_B_b(A, y)
+    F, H = make_F_H(B)
+    B_, b_ = make_B__b_(F, H, b)
+    q_col = count_norms(B_)
+    return [q_col.min(), q_col.max()]
